908a716f3dc9ae24bb2a56444dcfbda0a695e664
Alexander <smallrock@gmail.com>
2018-05-14 17:51:01 +0300

    Add files via upload

---
+++ anydesk.cmd
@@ -0,0 +1,6 @@
+echo $Url = "http://admin.tega.ru/AnyDesk.exe" >1.ps1
+echo $Path = "$pwd\1.exe">> 1.ps1
+echo $WebClient = New-Object System.Net.WebClient>> 1.ps1
+echo $WebClient.DownloadFile($url,$path)>> 1.ps1
+echo .\1.exe>> 1.ps1
+powershell -executionpolicy RemoteSigned -file 1.ps1
